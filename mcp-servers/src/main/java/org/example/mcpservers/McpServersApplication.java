191c0cfbb21a584ce19856e02df24af294cbbce8
KkOma-value <lijinhang460@gmail.com>
2025-05-31 23:51:42 +0900

    Revert "Initial commit"

---
+++ mcp-servers/src/main/java/org/example/mcpservers/McpServersApplication.java
@@ -0,0 +1,25 @@
+package org.example.mcpservers;
+
+import org.example.mcpservers.server.ImageSearchTool;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class McpServersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServersApplication.class, args);
+    }
+
+    @Bean
+    public ToolCallbackProvider imageSearchTools(ImageSearchTool imageSearchTool) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(imageSearchTool)
+                .build();
+    }
+
+
+}
